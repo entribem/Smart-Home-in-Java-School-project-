8431050eda3ab3053b89ea56eb02a1af2dd5b494
Petr Tuma <tumapet9@fel.cvut.cz>
2022-01-01 12:47:35 +0100

    Creating project.

---
+++ Smart_Home/src/main/java/smart_home/SmartHome.java
@@ -0,0 +1,23 @@
+package smart_home;
+
+/**
+ * Class SmartHome creates a simulation of a family living in a smart home.
+ * @author Petr Tuma, Mikhail Nalutka
+ */
+public class SmartHome {
+    
+    public static void main() {
+        
+        //Create House
+        
+        //Initialize People
+        
+        //Initialize Entities
+        
+        //Clock Simulate
+        
+        //Generate Reports
+        
+    }
+    
+}
