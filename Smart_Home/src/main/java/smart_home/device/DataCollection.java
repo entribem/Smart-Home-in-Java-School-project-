a1aff02235083f0a6377afdb38f0b8d2179b454c
Petr Tuma <tumapet9@fel.cvut.cz>
2022-01-02 15:16:52 +0100

    API for consumption.

---
+++ Smart_Home/src/main/java/smart_home/device/DataCollection.java
@@ -0,0 +1,9 @@
+package smart_home.device;
+
+import java.util.EnumSet;
+
+public interface DataCollection {
+    
+    public EnumSet<Resources> getResources();
+    
+}
---
+++ Smart_Home/src/main/java/smart_home/device/Resources.java
@@ -0,0 +1,35 @@
+package smart_home.device;
+
+public enum Resources {
+    Electricity("electricity", 0, "kWh"),
+    Gas("gas", 0, "kWh"),
+    Water("water", 0, "l"),
+    Durability("durability", 0, "%");
+    
+    private final String name;
+    private int value;
+    private final String unit;
+
+    private Resources(String name, int value, String unit) {
+        this.name = name;
+        this.value = value;
+        this.unit = unit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+    
+}
